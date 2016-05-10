f599dc34f80b39f0ebd0cdfd44854bc8337f0b46
Jeremy Toce <jeremytoce@gmail.com>
2016-05-09 20:40:00 -0700

    v3

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ var problem_data = [{
       'return false;' +
     '}'
   }
-}'},
+}},
 
   {
   name: 'Recursive Factorial',
@@ -24,14 +24,14 @@ var problem_data = [{
   code: 'function factorial(n){}',
   example: 'factorial(5) => 120 (1 * 2 * 3 * 4 * 5)',
   answer: 
-  'function factorial(n) {
-  if (n === 0) {
-    return 1;
-  }
-  else {
-    return n * factorial(n-1);
-  }
-}'}, 
+  'function factorial(n) {' +
+  'if (n === 0) {' +
+    'return 1;' +
+  '}' +
+  'else {' +
+    'return n * factorial(n-1);' +
+  '}' +
+}}, 
 
   {
   name: 'String Reverse',
@@ -45,61 +45,60 @@ var problem_data = [{
   'else {' +
     'return stringReverse(str.slice(1)) + str.charAt(0);' +
   '}'
-}'}, 
+}}, 
 
   {
   name: 'Array Sum',
   description: 'Compute the sum of all of the numbers in an array. Assume there is no array depth.',
   code: 'function arraySum(arr){}',
   example: 'arraySum([5,2,6]) -> 13',
-  answer: 
-  'function arraySum(arr) {
-  if (arr.length === 1) {
-    return arr[0];
-  }
-  else {
-    return arr[0] + arraySum(arr.slice(1));
-  }
-}'
+  answer: 'function arraySum(arr) {' + 
+  'if (arr.length === 1) {' +
+    'return arr[0];' +
+  '}' +
+  'else {' +
+    'return arr[0] + arraySum(arr.slice(1));' +
+  '}' +
+'}'
 }, 
 
-  {
-  name: 'Exponent Calculator',
-  description: 'Find the exponent of a number when the power is provided.',
-  code: 'function exp(base, exp){}',
-  example: 'exp(8,3) -> 62',
-  answer: 
+//   {
+//   name: 'Exponent Calculator',
+//   description: 'Find the exponent of a number when the power is provided.',
+//   code: 'function exp(base, exp){}',
+//   example: 'exp(8,3) -> 62',
+//   answer: 
 
-  'function exp(base, exp) {
-  if (exp === 0) {
-    return 1;
-  }
-  else {
-    return base * expon(base, exp-1);
-  }
-}'
-}, 
+//   'function exp(base, exp) {
+//   if (exp === 0) {
+//     return 1;
+//   }
+//   else {
+//     return base * expon(base, exp-1);
+//   }
+// }'
+// }, 
 
-  {
-  name: 'Even Number Check',
-  description: 'Write a recursive function that determines if a number n is even.',
-  code: 'function even(n){}',
-  example: 'even(7) -> false',
-  answer: 
-  'function even (n) {
-  if (n < 1) {
-    if (n === 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-  else {
-    return even2(n-2);
-  }
-}'
-},   
+//   {
+//   name: 'Even Number Check',
+//   description: 'Write a recursive function that determines if a number n is even.',
+//   code: 'function even(n){}',
+//   example: 'even(7) -> false',
+//   answer: 
+//   'function even (n) {
+//   if (n < 1) {
+//     if (n === 0) {
+//       return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   }
+//   else {
+//     return even2(n-2);
+//   }
+// }'
+// },   
 ];
 
 var Problems = React.createFactory(RecursionProblems);
